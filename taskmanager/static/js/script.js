a84fe6a7b80a33ebbb2a429710501b24983e4e62
CarlG96 <carl.gardiner1@hotmail.com>
2022-08-08 09:15:45 +0000

    commit

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,16 @@ document.addEventListener('DOMContentLoaded', function() {
     // Sidenav initialisation
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    //datepicker initialisation
+    let datePicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datePicker, {
+      date: "dd mmmm, yyyy",
+      i18n: {done: "Select"}
+    });
+
+    //select dropdown initialisation
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
   });
+
